49bac9ec17986c39f67469012605c2272f0f9b9f
Tejas Gawande <tejugawande25@gmail.com>
2023-12-28 17:32:19 +0530

    Digital Clock

---
+++ src/components/container/container.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./container.css";
+import { useState } from "react";
+import { useEffect } from "react";
+
+function Container() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    setInterval(() => setTime(new Date()));
+  }, 1000);
+
+  const formattedHours = time.getHours();
+  const formattedMinutes = time.getMinutes();
+  const formattedSeconds = time.getSeconds();
+  return (
+    <>
+      <div class="hero">
+        <div class="container">
+          <div class="clock hr">{(time.getHours<10?"0":" ")+formattedHours}</div>
+          <div class="clock min">{(formattedMinutes<10?"0":" ")+formattedMinutes}</div>
+          <div class="clock sec">{(formattedSeconds < 10 ?"0":"")+formattedSeconds}</div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Container;
---
+++ src/components/dashboard/dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Container from "../container/container";
+import "./dashboard.css"
+
+function Dashboard(){
+    return(<>
+    <Container />
+    </>);
+}
+export default Dashboard;

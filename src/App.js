014a814fad39f5d3fe2e7837d68e808e646f4ffb
Tejas Gawande <tejugawande25@gmail.com>
2023-12-28 17:41:41 +0530

    dashboard formatted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Dashboard from "./components/dashboard/dashboard";
 
-function App(){
-  return(<>
-  <Dashboard />
-  </>);
+function App() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
 }
 
 export default App;
